068ae7a3cce00a3a2a604c131afb7d35a1f1a3f2
leyuav <leyu.av@gmail.com>
2024-09-03 17:52:48 +0200

    cleaning up for GalleryHeaders

--- recruitment-task/src/components/GalleryHeaders.jsx
+++ recruitment-task/src/components/GalleryHeaders.jsx
@@ -1,26 +1,22 @@
 import styles from '../style';
 
 const GalleryHeaders = () => (
-  <section className='flex md:flex-row flex-col'>
-    <div className={`${styles.flexStart} flex-col pt-[80px] sm:px-[88px] px-8 w-[100%]`}>
-      <p
-        className={`${styles.paragraph} md:text-[21.5px] text-[16px] leading-[32.25px] text-accent`}
-      >
+  <section>
+    <div className={`${styles.flexStart} flex-col pt-[80px] sm:px-[88px] px-8`}>
+      <p className={`${styles.paragraph} md:text-[21.5px] text-[16px] mb-2 text-accent`}>
         Prezentacja firmy
       </p>
 
       <h1
-        className={`${styles.heading} mb-[24px] md:text-[40px] text-[26px] leading-[48px] tracking-[-0.002em]`}
+        className={`${styles.heading} mb-[24px] md:text-[40px] text-[26px] leading-[48px] tracking-[0.01em]`}
       >
         Zobacz naszą galerię zdjęć
       </h1>
 
       <div className='flex justify-start items-center'>
-        <button className={`${styles.buttonCategory} w-auto h-[23px] mr-[48px]`}>
-          Samochody osobowe
-        </button>
+        <button className={`${styles.buttonCategory} mr-[48px]`}>Samochody osobowe</button>
 
-        <button className={`${styles.buttonCategory} w-auto h-[23px]`}>Samochody dostawcze</button>
+        <button className={`${styles.buttonCategory}`}>Samochody dostawcze</button>
       </div>
     </div>
   </section>
